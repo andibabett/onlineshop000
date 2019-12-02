6a4a0c8c8ba18aafc25dcb5e0083472545b6b35a
andibabett <andi_babett@yahoo.com>
2019-12-02 19:00:36 +0200

    Initial commit

---
+++ src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
@@ -0,0 +1,73 @@
+package org.fasttrackit.onlineshop.service;
+
+import org.fasttrackit.onlineshop.domain.Product;
+import org.fasttrackit.onlineshop.exception.ResourceNotFoundException;
+import org.fasttrackit.onlineshop.persistance.ProductRepository;
+import org.fasttrackit.onlineshop.transfer.SaveProductRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(ProductService.class);
+
+    // IoC - Inversion of Control
+    private final ProductRepository productRepository;
+
+    // Dependency injection
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product createProduct(SaveProductRequest request) {
+        LOGGER.info("Creating product {} ", request);
+        Product product = new Product();
+        product.setDescription(request.getDescription());
+        product.setName(request.getName());
+        product.setPrice(request.getPrice());
+        product.setQuantity(request.getQuantity());
+        product.setImageUrl(request.getImageUrl());
+
+        return productRepository.save(product);
+    }
+
+    public Product getProduct(long id) {
+        LOGGER.info("Retrieving product {}", id);
+
+//        Optional<Product> productOptional = productRepository.findById(id);
+//
+//        if (productOptional.isPresent()) {
+//            return productOptional.get();
+//        } else {
+//            throw new ResourceNotFoundException("");
+//        }
+
+//        // using Optional
+        return productRepository.findById(id)
+                // lambda expression
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "Product " + id + " does not exist."));
+    }
+
+    public Product updateProduct(long id, SaveProductRequest request) {
+        LOGGER.info("Updating product {}: {}", id, request);
+
+        Product product = getProduct(id);
+
+        BeanUtils.copyProperties(request, product);
+
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(long id) {
+        LOGGER.info("Deleting product {}", id);
+        productRepository.deleteById(id);
+        LOGGER.info("Deleted product {}", id);
+    }
+}
---
+++ src/test/java/org/fasttrackit/onlineshop/ProductServiceIntegrationTests.java
@@ -0,0 +1,108 @@
+package org.fasttrackit.onlineshop;
+
+import org.fasttrackit.onlineshop.domain.Product;
+import org.fasttrackit.onlineshop.exception.ResourceNotFoundException;
+import org.fasttrackit.onlineshop.service.ProductService;
+import org.fasttrackit.onlineshop.transfer.SaveProductRequest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.TransactionSystemException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductServiceIntegrationTests {
+
+    @Autowired
+    private ProductService productService;
+
+    @Test
+    public void testCreateProduct_whenValidRequest_thenProductIsSaved() {
+        createProduct();
+    }
+
+    @Test(expected = TransactionSystemException.class)
+    public void testCreateProduct_whenInvalidRequest_thenThrowException() {
+        SaveProductRequest request = new SaveProductRequest();
+        // leaving request properties with default null values
+        // to validate the negative flow
+
+        productService.createProduct(request);
+    }
+
+    @Test
+    public void testGetProduct_whenExistingProduct_thenReturnProduct() {
+        Product createdProduct = createProduct();
+
+        Product product = productService.getProduct(createdProduct.getId());
+
+        assertThat(product, notNullValue());
+        assertThat(product.getId(), is(createdProduct.getId()));
+        assertThat(product.getName(), is(createdProduct.getName()));
+        assertThat(product.getPrice(), is(createdProduct.getPrice()));
+        assertThat(product.getQuantity(), is(createdProduct.getQuantity()));
+        assertThat(product.getDescription(), is(createdProduct.getDescription()));
+    }
+
+    @Test(expected = ResourceNotFoundException.class)
+    public void testGetProduct_whenNonExistingProduct_thenThrowResourceNotFoundException() {
+        productService.getProduct(9999999999L);
+    }
+
+    @Test
+    public void testUpdateProduct_whenValidRequest_thenReturnUpdatedProduct() {
+        Product createdProduct = createProduct();
+
+        SaveProductRequest request = new SaveProductRequest();
+        request.setName(createdProduct.getName() + " updated");
+        request.setDescription(createdProduct.getDescription() + " updated");
+        request.setPrice(createdProduct.getPrice() + 10);
+        request.setQuantity(createdProduct.getQuantity() + 10);
+
+        Product updatedProduct =
+                productService.updateProduct(createdProduct.getId(), request);
+
+        assertThat(updatedProduct, notNullValue());
+        assertThat(updatedProduct.getId(), is(createdProduct.getId()));
+        assertThat(updatedProduct.getName(), is(request.getName()));
+        assertThat(updatedProduct.getDescription(), is(request.getDescription()));
+        assertThat(updatedProduct.getPrice(), is(request.getPrice()));
+        assertThat(updatedProduct.getQuantity(), is(request.getQuantity()));
+    }
+
+    @Test(expected = ResourceNotFoundException.class)
+    public void testDeleteProduct_whenExistingProduct_thenProductIsDeleted(){
+        Product product = createProduct();
+
+        productService.deleteProduct(product.getId());
+
+        productService.getProduct(product.getId());
+    }
+
+    private Product createProduct() {
+        SaveProductRequest request = new SaveProductRequest();
+        request.setName("Banana " + System.currentTimeMillis());
+        request.setPrice(5.0);
+        request.setQuantity(100);
+        request.setDescription("Healthy food");
+
+        Product createdProduct = productService.createProduct(request);
+
+        assertThat(createdProduct, notNullValue());
+        assertThat(createdProduct.getId(), notNullValue());
+        assertThat(createdProduct.getId(), greaterThan(0L));
+        assertThat(createdProduct.getName(), is(request.getName()));
+        assertThat(createdProduct.getPrice(), is(request.getPrice()));
+        assertThat(createdProduct.getQuantity(), is(request.getQuantity()));
+        assertThat(createdProduct.getDescription(), is(request.getDescription()));
+
+        return createdProduct;
+    }
+}
